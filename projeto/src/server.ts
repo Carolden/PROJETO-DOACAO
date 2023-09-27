bda0173515cf422328d7271d63b2374c1b14d52b
0ld3nburg <ceduburg@gmail.com>
2023-09-26 21:40:41 -0300

    routes e server

---
+++ projeto/src/server.ts
@@ -0,0 +1,20 @@
+// import express, { Express } from 'express';
+// import cors from 'cors';
+
+
+
+// let server: Express = express();
+
+// server.use(cors());
+// server.use(express.json());
+
+// // server.use(usuariosRoutes);
+// // server.use(produtosRoutes);
+
+// export default {
+//   start () {
+//     server.listen(3000, () => {
+//       console.log('Server started on port 3000!');
+//     });
+//   }
+// };
